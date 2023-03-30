866edd8fbff4066b3be2fd29165735876d19ef9c
Gaëtan André <rvlander@gaetanandre.eu>
2023-03-30 14:29:32 +0200

    feat: initialize maven project

---
+++ swarm_melee/src/main/java/eu/rvlander/swarm_melee/App.java
@@ -0,0 +1,13 @@
+package eu.rvlander.swarm_melee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ swarm_melee/src/test/java/eu/rvlander/swarm_melee/AppTest.java
@@ -0,0 +1,20 @@
+package eu.rvlander.swarm_melee;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
